284255da1cf18f81e2551fdf30fe1679571149a2
Siri <sirilakku01@gmail.com>
2023-05-11 18:47:34 -0600

    Added Home end point

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/login/Login";
 
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/home' element={<Home />}  />
-      </Routes>
-      </BrowserRouter>  
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Login />}/>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,11 @@
-import { ThemeProvider } from '@emotion/react';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <ul >
-        <li><a href='#'>Home</a></li>
+      <ul>
+        <li><a href='/home'>Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Services</a></li>
         <li><a href="#">Contact</a></li>
