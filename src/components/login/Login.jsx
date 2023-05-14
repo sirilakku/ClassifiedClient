1d7ec871af0d8de56d2c0c8a2a8db1f299e54daf
Siri <sirilakku01@gmail.com>
2023-05-13 18:22:04 -0600

    Added code for login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 import './Login.css'; // Import the CSS file
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -13,10 +17,25 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
-  const handleLogin = () => {
-    console.log('Username:', username);
-    console.log('Password:', password);
+  const handleLogin = async() => {
+    const user = { username: username, password: password };
+    const data = JSON.stringify(user);
+
+    const response = await fetch('http://localhost:8080/loginUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    })
+    if (response.status === 200) {
+      console.log(response);
+      navigate("/home");
+    } else {
+      alert("Login Failed");
+    }
   };
+
 
   return (
     <div className="container">
@@ -46,50 +65,3 @@ const Login = () => {
 };
 
 export default Login;
-
-
-// import React, { useState, useEffect } from "react";
-
-// import { GoogleLogin, GoogleLogout } from 'react-google-login';
-
-// const Login = () => {
-//     const clientId = "920068830599-o9v3hshtbe58nnthed1g68859n15151t.apps.googleusercontent.com";
-    
-//     const [showloginButton, setShowloginButton] = useState(true);
-//     const [showlogoutButton, setShowlogoutButton] = useState(false);
-//     const onLoginSuccess = (res) => {
-//                 console.log('Login Success:', res.profileObj);
-//                 setShowloginButton(false);
-//                 setShowlogoutButton(true);
-//             };
-        
-//             const onLoginFailure = (res) => {
-//                 console.log('Login Failed:', res);
-//             };
-        
-//             const onSignoutSuccess = () => {
-//                 alert("You have been logged out successfully");
-//                 console.clear();
-//                 setShowloginButton(true);
-//                 setShowlogoutButton(false);
-//             };
-//   return (
-//     // <div>Login Page</div>
-//     <div>
-//         <h1>Login</h1>
-//         <GoogleLogin
-//     clientId={clientId}
-//     buttonText="Login with Google"
-//     // onSuccess={responseGoogle}
-//     // onFailure={responseGoogle}
-//     cookiePolicy={'single_host_origin'}
-//   />
-//     </div>
-//   )
-// }
-
-// export default Login
-
-
-
-
